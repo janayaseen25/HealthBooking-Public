fdc1eaff9576aad1ae878eeb7bef37a2eb9b5455
janayaseen25 <jana.yaseen@outlook.com>
2026-06-12 18:41:25 +0300

    Update BookAppointment.vue

--- views/BookAppointment.vue
+++ views/BookAppointment.vue
@@ -47,7 +47,7 @@ export default {
     };
   },
   mounted() {
-    fetch("https://e2m2b7y8c9.execute-api.us-east-1.amazonaws.com/prod/slots")
+    fetch("https://l6pzo5p4xh.execute-api.eu-north-1.amazonaws.com")
       .then(res => res.json())
       .then(data => {
         const parsed = JSON.parse(data.body);
@@ -62,7 +62,7 @@ export default {
         slot: this.selectedSlot
       };
 
-      fetch("https://e2m2b7y8c9.execute-api.us-east-1.amazonaws.com/prod/appointments", {
+      fetch("https://l6pzo5p4xh.execute-api.eu-north-1.amazonaws.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ body: JSON.stringify(payload) })
